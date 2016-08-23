3682575db571b92b3f5021d55f71d40c4720bf05
adrienkwong <lampardkwong@gmail.com>
2016-08-24 00:05:12 +0800

    24 Aug update
    24 Aug update at 00:05am

--- app/pages/bet/bet.ts
+++ app/pages/bet/bet.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/bet/bet.html'
 })
-export class AboutPage {
+export class BetPage {
   constructor(private navCtrl: NavController) {
   }
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
+import {BetPage} from '../bet/bet';
+import {SettingsPage} from '../settings/settings';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -16,7 +16,7 @@ export class TabsPage {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.tab2Root = BetPage;
+    this.tab3Root = SettingsPage;
   }
 }

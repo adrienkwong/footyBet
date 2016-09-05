87445742782ef039d57ba101cffef010c18f8adb
adrienkwong <lampardkwong@gmail.com>
2016-09-05 11:56:46 +0800

    Success in passing the name to Modal page. How about betting histroy for each match?
    Success in passing the name to Modal page. How about betting histroy for
    each match?

--- app/pages/history/history.ts
+++ app/pages/history/history.ts
@@ -9,9 +9,20 @@ import {Data} from '../../providers/data/data'
 })
 export class HistoryPage {
 
+  public better: any;
+
   constructor(private navCtrl: NavController, public viewCtrl: ViewController, public dataService: Data, private navParams: NavParams) {
 
+
   	console.log(this.navParams.get('better'));
+
+    this.better = this.navParams.get('better');
+
+    console.log("2nd", this.better.name);
+
+
+
+
 
 	  this.dataService.getMatchesData().then((data) => {
 	        console.log("MATCHES DATA: ", data);
